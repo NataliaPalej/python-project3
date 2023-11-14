4d054daff3731e5bfc0ddad6b028fff94fc85817
Natalia <PalejNatalia@gmail.com>
2023-11-14 15:40:44 +0000

    Started implementing app.py file with connection to db

---
+++ NPalej_A00279259/app.py
@@ -0,0 +1,17 @@
+"""
+A sample Hello World server.
+"""
+import os
+
+from flask import Flask, request, flash, url_for, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dogs.sqlite3'
+app.secret_key = 'super secret key'
+db = SQLAlchemy(app)
+
+
+if __name__ == '__main__':
+    server_port = os.environ.get('PORT', '8080')
+    app.run(debug=False, port=server_port, host='0.0.0.0')
